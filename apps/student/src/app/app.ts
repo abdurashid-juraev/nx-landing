423a3f2616bcd06a4a418abb8188b03a2d9389a0
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2026-03-02 07:01:47 +0500

    fix taiilwind and primeng

--- apps/student/src/app/app.ts
+++ apps/student/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { BtnComponent } from '../../../../libs/shared/ui/src/lib/shared/buttons/btn';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, BtnComponent, ButtonModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ libs/shared/ui/src/lib/shared/buttons/btn.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-btn',
+  template:`<button class="bg-amber-500">submit</button>`
+})
+
+export class BtnComponent {
+
+}
