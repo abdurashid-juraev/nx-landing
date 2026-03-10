17863ca100f6c061ee07446a71a5dcf41112f13c
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2026-03-10 05:14:03 +0500

    fix libs

---
+++ libs/admin/data-access-auth/src/lib/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor() {}
+
+  login() {
+    // API ga so'rov yuboradigan logika shu yerda bo'ladi
+  }
+}
