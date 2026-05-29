7831b6f72bc51ae081d6a8de9b87cbfc015d3f70
abdurashid-juraev <aj6243434@gmail.com>
2026-05-29 08:11:22 +0500

    fix auth

--- libs/admin/feature-auth/src/lib/feature-auth/feature-auth.ts
+++ libs/admin/feature-auth/src/lib/feature-auth/feature-auth.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@org/data-access-auth';
 @Component({
   selector: 'lib-feature-auth',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './feature-auth.html',
   styleUrl: './feature-auth.css'
 })
-export class FeatureAuth {}
+export class FeatureAuth {
+  private readonly formBuildder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  protected readonly loginForm = this.formBuildder.nonNullable.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+}
